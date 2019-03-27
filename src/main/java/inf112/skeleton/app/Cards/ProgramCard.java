f6c4cb5fb8501361cdda20aa2ded64a35ca7885b
Matias Vidnes <matias@vidnes.net>
2019-03-27 13:32:33 +0100

    created programCardsComparable test, and fixed programCards equalmethod to.

--- src/main/java/inf112/skeleton/app/Cards/ProgramCard.java
+++ src/main/java/inf112/skeleton/app/Cards/ProgramCard.java
@@ -37,7 +37,8 @@ public class ProgramCard implements ICard {
 
 	@Override
 	public boolean equals(Object obj) {
-		return programType == ((ProgramCard) obj).getProgramType();
+
+		return programType == ((ProgramCard) obj).getProgramType() && priority == ((ProgramCard) obj).getPriority();
 	}
 
 	@Override
