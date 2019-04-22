16f640e5d0f60451c7817625a68475cb44d514df
Sondre Bolland <sondrebolland@hotmail.com>
2019-04-22 15:45:32 +0200

    Added life token assets

--- assets/testTileset.tsx
+++ assets/testTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="testTileset" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="testTilesetv22.png" width="320" height="327"/>
+ <image source="testTilesetv24.png" width="320" height="327"/>
 </tileset>
