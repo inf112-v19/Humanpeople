11757387733f909f24e7c270c14c1a3e146d4cb9
Matias Vidnes <matias@vidnes.net>
2019-05-01 12:12:56 +0200

    changed ip again

--- src/main/java/inf112/skeleton/app/Networking/GameClient.java
+++ src/main/java/inf112/skeleton/app/Networking/GameClient.java
@@ -28,7 +28,7 @@ public class GameClient {
     private int timeToWaitForServerToRespond = 5000;
 
     //J-option ping, what would you like to connect to
-    private String IPAddress = "10.113.28.4";
+    private String IPAddress = "192.168.56.1";
     private boolean connected;
     private Player player;
     RoboRally game;
